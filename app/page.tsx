72245b670d27452b388ca7e5ad0bc1cdcb13aa3e
tomizdebski <tomizdebski@gmail.com>
2023-09-26 15:52:58 +0200

    first_component_gen

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import Chat from '@/components/Chat'
+import Login from '@/components/Login'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+      {/* <Login /> */}
+      <Chat />
     </main>
   )
 }
