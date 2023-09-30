6c12e0dd3f8a7f45b5cf37bf5abc58a73f143be7
tomizdebski <tomizdebski@gmail.com>
2023-09-30 21:42:59 +0200

    remove_auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 "use client";
-import Image from "next/image";
 import socketIO from "socket.io-client";
 import { useContext, useState, useEffect } from "react";
 import { SocketContext } from "@/context/SocketContext";
-import { IMessage } from "@/types/types";
 import Login from "@/components/Login";
 
 //const socket = socketIO("http://localhost:4000");
